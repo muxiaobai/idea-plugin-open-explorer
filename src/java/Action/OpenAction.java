d7a8a2e3079b621e827988ee5c57c2782dd830de
zhangpengfei <zpf12345678910@gmail.com>
2021-04-25 14:10:58 +0800

    idea 插件 自动在explorer中打开文件路径，或者打开编译后的文件路径

---
+++ src/java/Action/OpenAction.java
@@ -0,0 +1,29 @@
+package java.Action;
+
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ *
+ * @author muxiaobai
+ */
+public class OpenAction extends AnAction {
+    // 如果通过Java代码来注册，这个构造函数会被调用，传给父类的字符串会被作为菜单项的名称
+    // 如果你通过plugin.xml来注册，可以忽略这个构造函数
+    public OpenAction() {
+        // 设置菜单项名称
+        super("Text _Boxes");
+        // 还可以设置菜单项名称，描述，图标
+        // super("Text _Boxes","Item description",IconLoader.getIcon("/Mypackage/icon.png"));
+    }
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
